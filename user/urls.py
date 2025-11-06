2b5c729ad017316389510efd8e6ef32146212b41
mehdizhd11 <98504734+mehdizhd11@users.noreply.github.com>
2025-11-06 19:07:27 +0330

    edit user: (create a customer view,serializer,url)

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import CustomTokenObtainPairView, CustomerSignUpView, TestAuthenticationView,RestaurantSignUpView, ChangePasswordView
+
+urlpatterns = [
+    path('signup/restaurant', RestaurantSignUpView.as_view(), name='restaurant_signup'),
+    path('token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/customer', CustomerSignUpView.as_view(), name='customer_signup'),
+    path('test-auth', TestAuthenticationView.as_view(), name='test-auth'),
+    path('change-password', ChangePasswordView.as_view(), name='change-password'),
+
+]
